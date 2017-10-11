ea12638fa1bd4891c8048bb688a3b989710f82f3
dmitriy <pro.diyezz@yahoo.com>
2017-10-11 18:50:34 +0300

    added angular to site

--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { IndexComponent } from './pages/index/index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule
