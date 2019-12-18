35fef21b3396d8c57807456faeba0d0d6240b6fe
diyezz <pro.diyezz@yahoo.com>
2019-12-18 13:24:50 +0200

    Small fix.

--- backend/functions/src/controllers/contacts.ts
+++ backend/functions/src/controllers/contacts.ts
@@ -3,14 +3,14 @@ import * as fs from 'fs';
 
 export default class ContactsController {
   static TTOKEN = '1039070964:AAEHjR1rUY7ZFH2KLreHub8LQdbGpcbYZOI';
-  static PUBLICCHATID = '@bragarchukarchitect';
+  static TPUBLICCHATID = '@bragarchukarchitect';
   static TOPTIONS = { polling: true };
 
   // Initialize Telegram Bot
-  static bot: any = new TelegramBot(ContactsController.TTOKEN, ContactsController.TOPTIONS);;
+  static bot: any = new TelegramBot(ContactsController.TTOKEN, ContactsController.TOPTIONS);
 
   static async getChatInfo(req: any, res: any) {
-    const chatID = ContactsController.PUBLICCHATID;
+    const chatID = ContactsController.TPUBLICCHATID;
     const bot = ContactsController.bot;
 
     try {
@@ -22,16 +22,16 @@ export default class ContactsController {
   }
 
   static async uploadContact(req: any, res: any) {
-    const chatID = ContactsController.PUBLICCHATID;
+    const chatID = ContactsController.TPUBLICCHATID;
     const bot = ContactsController.bot;
+    const contact = JSON.parse(req.body);
+    const message = `New contact! \nName: ${contact.firstName}, \nSurname: ${contact.lastName}, \nEmail: ${contact.email}`;
 
-    fs.appendFile('./contacts.txt', ', \n' + JSON.stringify(JSON.parse(req.body), null, 2), () => {
+    fs.appendFile('./contacts.txt', ', \n' + JSON.stringify(contact, null, 2), () => {
       console.log('File written successfully!');
     });
 
     try {
-      const contact = JSON.parse(req.body);
-      const message = `New contact! \nName: ${contact.firstName}, \nSurname: ${contact.lastName}, \nEmail: ${contact.email}`;
       await bot.sendMessage(chatID, message);
     } catch (error) {
       console.error(error);
